acd368f2a359a35f375c3e2b1d6a3a665878eb01
Lautirojas <103603017+Lautirojas@users.noreply.github.com>
2026-05-27 17:13:49 -0300

    Release v1.0.0

--- Plugins/BuildingFramework/Source/BuildingFramework/Public/Items/Definitions/ItemDefinition.h
+++ Plugins/BuildingFramework/Source/BuildingFramework/Public/Items/Definitions/ItemDefinition.h
@@ -3,14 +3,14 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
-#include "Items/Fragments/PFItemFragment.h"
-#include "PFItemDefinition.generated.h"
+#include "Items/Fragments/ItemFragment.h"
+#include "ItemDefinition.generated.h"
 
 // Forward Declarations
-class UPFItemInstance;
+class UItemInstance;
 
 UCLASS(BlueprintType)
-class BUILDINGFRAMEWORK_API UPFItemDefinition : public UPrimaryDataAsset
+class BUILDINGFRAMEWORK_API UItemDefinition : public UPrimaryDataAsset
 {
     GENERATED_BODY()
 
